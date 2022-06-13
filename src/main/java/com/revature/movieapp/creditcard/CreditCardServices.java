ec6b59b38d3cf13e89d5d2a897917c48f59414d2
Matthew Swaney <Matthew.J.Swaney@gmail.com>
2022-06-13 13:59:14 -0500

    updates to creditcardservices adding the validateinput method

--- src/main/java/com/revature/movieapp/creditcard/CreditCardServices.java
+++ src/main/java/com/revature/movieapp/creditcard/CreditCardServices.java
@@ -1,10 +1,12 @@
 package com.revature.movieapp.creditcard;
 import com.revature.movieapp.creditcard.CreditCard;
+import com.revature.movieapp.customer.Customer;
 import com.revature.movieapp.customer.CustomerDao;
 import com.revature.movieapp.util.interfaces.Serviceable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.*;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -45,7 +47,13 @@ public class CreditCardServices implements Serviceable<CreditCard> {
     }
 
     @Override
-    public boolean validateInput(CreditCard object) {
-        return false;
+    public boolean validateInput(CreditCard newCreditCard) {
+        if (newCreditCard.getCc_number() == null || newCreditCard.getCc_number().trim().equals("")){return false;}
+        if (newCreditCard.getCc_name() == null || newCreditCard.getCc_name().trim().equals("")){return false;}
+        if (newCreditCard.getCvv() < 99 || newCreditCard.getCvv() > 9999) {return false;}
+        if (newCreditCard.getExp_date() == null || newCreditCard.getExp_date().equals("")){return false;}
+        if (newCreditCard.getZip() < 9999 || newCreditCard.getZip() > 99999) {return false;}
+        if (newCreditCard.getCredit_limit() <= 0) {return false;}
+        return true;
     }
 }
