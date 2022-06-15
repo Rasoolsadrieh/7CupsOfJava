ca594cddcafd104f2e3e102c9839a2677f2a2f35
Matthew Swaney <Matthew.J.Swaney@gmail.com>
2022-06-15 10:50:56 -0500

    added @Modifying to update and insert custom HQL commands in creditcarddao and orderdao

--- src/main/java/com/revature/movieapp/creditcard/CreditCardDao.java
+++ src/main/java/com/revature/movieapp/creditcard/CreditCardDao.java
@@ -1,6 +1,7 @@
 package com.revature.movieapp.creditcard;
 
 import com.revature.movieapp.order.Order;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import java.util.Optional;
 @Repository
 public interface CreditCardDao extends CrudRepository<CreditCard, String> {
 
+    @Modifying
     @Query(value = "Update CreditCard set credit_limit= -3 where cc_number= :cc_number")
     Optional<CreditCard> completePayment(String cc_number);
 
--- src/main/java/com/revature/movieapp/order/OrderDao.java
+++ src/main/java/com/revature/movieapp/order/OrderDao.java
@@ -2,6 +2,7 @@ package com.revature.movieapp.order;
 
 import com.revature.movieapp.creditcard.CreditCard;
 import com.revature.movieapp.order.Order;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,9 +13,11 @@ import java.util.Optional;
 public interface OrderDao extends CrudRepository<Order, Integer> {
 
 
+    @Modifying
     @Query(value = "INSERT into Order values id= :id, movieID = :movieID, rent, orderDate= :orderDate, returnDate= :returnDate, 0, 3, email= :email")
     Optional<Order> rentMovie(int id, String movieID,String orderDate, String returnDate, String email);
 
+    @Modifying
     @Query(value = "Update Order set isComplete= 1 where id= :id")
     Optional<Order> completeOrder(int id);
 
